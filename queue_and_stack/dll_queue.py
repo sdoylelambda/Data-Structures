c08f8cb9a67c86f9d2be56775d5e1c9474a9b6bb
Sean Doyle <sdoylelambda@gmail.com>
2019-12-17 09:48:55 -0500

    First test on stack.py passes.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -19,19 +19,22 @@ class Queue:
         # self.size.storage = self.storage.len()
         self.size += 1
         # return?
-        self.storage.add_to_head(value)
+        return self.storage.add_to_tail(value)
 
 # DELETE
     def dequeue(self):
-        self.size - 1
-        node = storage.
-        self.storage.delete(node)
+        if self.size >= 1:
+            self.size -= 1
+            self.storage.remove_from_head()
+        else:
+            return
         # print('self.storage.pop()')
         # print(self.storage.pop())
         # return self.storage.pop()
 
     def len(self):
-        self.storage.get_max()
+        return self.size
+        # return self.storage.get_max()
         # print('len(self.storage)')
         # print(len(self.storage))
         # return len(self.storage)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,17 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
 
     def push(self, value):
-        pass
+        self.size += 1
+        while len(self.storage) != 0:
+            self.storage.add_to_head(value)
+            self.storage.pop()
 
     def pop(self):
         pass
 
     def len(self):
-        pass
+        return self.size
